3c4a63a3e880f0c31165da54fd949961ad31606e
Jesús Checa <jesuscheca11@gmail.com>
2023-06-09 02:13:16 +0200

    Creación máquinas turbofy

--- mynetwork.tf
+++ mynetwork.tf
@@ -3,14 +3,14 @@
 
 # Create the mynetwork network
 resource "google_compute_network" "mynetwork" {
-  name                    = "mynetwork-tf"
+  name                    = "turbofynet-tf"
   auto_create_subnetworks = "true"
   project                 = var.gcp_project
 }
 
 # Add a firewall rule to allow HTTP, SSH, RDP, and ICMP traffic on mynetwork
 resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
-  name    = "mynetwork-tf-allow-http-ssh-rdp-icmp"
+  name    = "turbofynet-tf-allow-http-ssh-rdp-icmp"
   network = google_compute_network.mynetwork.name
   allow {
     protocol = "tcp"
@@ -24,23 +24,21 @@ resource "google_compute_firewall" "mynetwork-allow-http-ssh-rdp-icmp" {
     protocol = "icmp"
   }
 }
-
-# Create the jenkins-vm instance
-module "jenkins-vm" {
+  
+module "turbofy-back-vm" {
   source          = "./instance"
-  instance_name   = "jenkins-vm-tf"
+  instance_name   = "turbofy-back-vm-tf"
   instance_region = "us-central1"
   instance_zone   = "us-central1-a"
-  instance_type   = "n1-standard-2"
-  image           = "ubuntu-os-cloud/ubuntu-2004-lts" # ubuntu-1804-lts"
-  #  startup_script      = "${var.init_scrip_docker}"
+  instance_type   = "n1-standard-1"
+  image           = "ubuntu-os-cloud/ubuntu-2004-lts"  #ubuntu-1804-lts"  
+  #  startup_script      = "${var.init_scrip_apache2}"
   instance_subnetwork = google_compute_network.mynetwork.self_link
 }
-
-# Create the web-deploy-vm" instance
-module "web-deploy-vm" {
+  
+module "turbofy-front-vm" {
   source          = "./instance"
-  instance_name   = "web-deploy-vm-tf"
+  instance_name   = "turbofy-front-vm-tf"
   instance_region = "us-central1"
   instance_zone   = "us-central1-a"
   instance_type   = "n1-standard-1"
@@ -51,16 +49,16 @@ module "web-deploy-vm" {
 
 
 resource "null_resource" "execute" {
-
+    
   provisioner "remote-exec" {
     connection {
-      host        = module.jenkins-vm.instance_ip_addr
+      host        = module.turbofy-front-vm.instance_ip_addr
       type        = "ssh"
       user        = "ubuntu"
       private_key = file("~/.ssh/id_rsa")
     }
 
-    ## Script inicialización jenkins-vm
+    ## Script inicialización web-deploy-vm
     inline = [
       "sudo apt-get update -y",
       "echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections",
@@ -68,21 +66,31 @@ resource "null_resource" "execute" {
       "sudo apt-get upgrade -y",
       "sudo apt-get install -y python-minimal",
       "sudo timedatectl set-timezone Europe/Madrid",
-      # Instalacion de docker
+      # Instalacion de docker       
       "sudo apt install apt-transport-https ca-certificates curl software-properties-common -y",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
       "sudo add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable'",
       "sudo apt update -y",
       "sudo apt install docker-ce docker-ce-cli containerd.io -y",
       "sudo usermod -aG docker $${USER}",
+      # Instalacion de docker composer
+      "sudo curl -L https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      # Instalacion de Java jdk 8
+      "sudo apt install openjdk-8-jdk -y",
+      "echo JAVA_HOME=\"/usr/lib/jvm/java-8-openjdk-amd64/jre\" | sudo tee -a /etc/environment",
+      # Instalacion de Node JS 16
+      "curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -",
+      "sudo apt-get install -y nodejs",
+      "sudo apt-get install npm -y"
+
     ]
     on_failure = continue
   }
-
-
+      
   provisioner "remote-exec" {
     connection {
-      host        = module.web-deploy-vm.instance_ip_addr
+      host        = module.turbofy-back-vm.instance_ip_addr
       type        = "ssh"
       user        = "ubuntu"
       private_key = file("~/.ssh/id_rsa")
@@ -120,6 +128,6 @@ resource "null_resource" "execute" {
   depends_on = [
     # Init script must be created before this IP address could
     # actually be used, otherwise the services will be unreachable.
-    module.web-deploy-vm.instance_ip_addr, module.jenkins-vm.instance_ip_addr
+    module.turbofy-back-vm.instance_ip_addr, module.turbofy-front-vm
   ]
 }
